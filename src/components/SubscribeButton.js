66ee371ce0d5436942238fd9ae38cdbda5da076c
garrettprince <garrettjamesprince@gmail.com>
2023-05-10 09:38:15 -0700

    more category spacing changes

--- src/components/SubscribeButton.js
+++ src/components/SubscribeButton.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function SubscribeButton() {
   return (
-    <Link href='/subscribe' className="subscribe-shadow latest-shadow-2 subscribe-word-shadow font-mono text-xs bg-gradient-to-tr from-cta-gradient-1 to-cta-gradient-2 h-5 rounded-md px-2 pt-[.1rem] py-0 mr-1 mb-[.4rem]">
+    <Link href='/subscribe' className="subscribe-shadow latest-shadow-2 subscribe-word-shadow font-mono text-xs bg-gradient-to-tr from-cta-gradient-1 to-cta-gradient-2 h-5 rounded-md px-2 pt-[.15rem] py-0 mr-1 mb-[.4rem]">
       SUBSCRIBE
     </Link>
   );
