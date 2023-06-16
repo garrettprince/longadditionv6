4cb4345699b56188736ac348a1c4ca96d3e41167
garrettprince <garrettjamesprince@gmail.com>
2023-06-16 20:43:58 +0800

    adding analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import Layout from "/src/components/layout.js";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout className={``}>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
